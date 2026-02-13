479ce7361141d735519c156fc9fbfdca6b15ef1c
Irina <viavgust@gmail.com>
2026-02-13 00:01:45 -0500

    Fix studio layout hydration

--- src/app/studio/[[...tool]]/layout.tsx
+++ src/app/studio/[[...tool]]/layout.tsx
@@ -7,9 +7,5 @@ export default function StudioLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  )
+  return children
 }
